4ac486710e6f0f9d941582a63acbec1952f3c2a7
Tamas Csabina <tcsabina@yahoo.com>
2023-01-31 21:24:32 +0100

    Adding the ezquake build, and testing with an updated qwprot file

--- src/protocol.h
+++ src/protocol.h
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define __PROTOCOL_H__
 
 #define	PROTOCOL_VERSION	28
-
+#error stop-this-now
 //=========================================
 
 // fte protocol extensions.
